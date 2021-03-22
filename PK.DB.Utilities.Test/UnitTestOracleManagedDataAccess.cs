86023f010a8c8cd8b41ca6b0b2cc055d7153826d
PK <lqs.pk@qq.com>
2021-03-22 16:50:35 +0800

    增加Oracle数据库帮助类

---
+++ PK.DB.Utilities.Test/UnitTestOracleManagedDataAccess.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using PK.DB.Utilities.Oracle.Helpers;
+
+namespace PK.DB.Utilities.Test
+{
+    [TestClass]
+    public class UnitTestOracleManagedDataAccess
+    {
+        private readonly OracleManagedDataAccessHelper helper = new OracleManagedDataAccessHelper("Data Source=oracle;User Id=oracleuser;Password=pwd");
+        [TestMethod]
+        public void TestExecuteQuerySql()
+        {
+            var dt = helper.ExecuteQuerySql("select * from v$version");
+        }
+    }
+}
---
+++ PK.DB.Utilities/Oracle/Helpers/OracleManagedDataAccessHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Oracle.ManagedDataAccess.Client;
+
+namespace PK.DB.Utilities.Oracle.Helpers
+{
+    public class OracleManagedDataAccessHelper : IDisposable
+    {
+        /// <summary>
+        /// 数据库链接
+        /// </summary>
+        private readonly OracleConnection _connection = null;
+
+        public OracleConnection Connection
+        {
+            get { return _connection; }
+        }
+
+        public OracleManagedDataAccessHelper(string connectionString)
+        {
+            _connection = new OracleConnection(connectionString);
+        }
+
+        public OracleManagedDataAccessHelper(string connectionString, OracleCredential orclCredential)
+        {
+            _connection = new OracleConnection(connectionString, orclCredential);
+        }
+
+        public void Dispose()
+        {
+            if (_connection != null)
+            {
+                _connection.Dispose();
+            }
+        }
+
+        public DataTable ExecuteQuerySql(string sql)
+        {
+            var da = new OracleDataAdapter(sql, _connection);
+            var dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+    }
+}
